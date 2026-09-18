0d9152e855a5dc7827ba2ece3cda249c60a0d49e
Kakul Rajput <rajputkakul0712@gmail.com>
2026-09-18 16:17:00 +0530

    "Animated player added"

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pygame
 pygame.init()
+
 screen = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Treasure Hunt")
+
+player_image = pygame.image.load("assets/player.png")
+player_image = pygame.transform.scale(player_image, (50, 50))
 player = pygame.Rect(100, 200, 50, 50)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
       player.x += 5
@@ -19,6 +25,9 @@ while running:
       player.y += 5 
 
     screen.fill((30, 30, 40))
-    pygame.draw.rect(screen, (50, 200, 100), player)
+
+    screen.blit(player_image, player)
+
     pygame.display.update()
+
 pygame.quit()
